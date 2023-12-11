5caa0b87a83fcc2a77e761a47a2d4481a9b0a550
Ayaz Uddin <ayazuddin@ayazs-air.mynetworksettings.com>
2023-12-11 17:31:34 -0500

    add projects

--- components/section-heading/index.tsx
+++ components/section-heading/index.tsx
@@ -1,5 +1,9 @@
 import React, { ReactNode } from "react";
 
 export default function SectionHeading({ children }: { children: ReactNode }) {
-  return <p className="text-[2rem] font-medium capitalize mb-8">{children}</p>;
+  return (
+    <p className="text-center text-[2rem] font-medium capitalize mb-8">
+      {children}
+    </p>
+  );
 }
