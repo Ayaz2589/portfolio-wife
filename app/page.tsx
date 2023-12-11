6f1efd791fd2aef6c48b1dcae00416c405131d4f
Ayaz Uddin <ayazuddin@ayazs-air.mynetworksettings.com>
2023-12-11 13:33:25 -0500

    fix mobile padding

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Intro } from "@/components";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between pt-24 md:p-24">
       <Intro />
     </main>
   );
