179427b3e041ada496299544efb320a042cca6ce
Ayaz Uddin <ayazuddin@ayazs-air.mynetworksettings.com>
2023-12-13 17:58:49 -0500

    update copy

--- components/about-me/index.tsx
+++ components/about-me/index.tsx
@@ -31,19 +31,14 @@ export default function AboutMe() {
         <span className="font-medium">
           React, Next.js, Node.js, and MongoDB
         </span>
-        . I am also knowledgable with TypeScript, Prisma and Material UI. I am
-        always looking to learn new technologies. I am currently looking for a{" "}
-        <span className="font-medium">full-time position</span> as a software
-        developer.
+        . I am also knowledgable with TypeScript, Prisma and Material UI.
       </p>
       <p>
-        <span className="italic">When I'm not coding</span>, I enjoy sketching
-        and painting, and taking trips with my wife.{" "}
-        <span className="font-medium">I love learning new things</span>. I am
-        currently learning{" "}
-        <span className="font-medium">CAD and 3D Printing</span>. I'm also
-        taking an intermidiate to advanced{" "}
-        <span className="font-medium">sketching course.</span>
+        During my tenture as a programmer I had the oportunity to work on large{" "}
+        <span className="font-medium">distributed teams</span> as well as{" "}
+        <span className="font-medium">smaller startup teams</span>. My work
+        includes building backend services such as large idenity services as
+        well as client side application components and flows.
       </p>
     </motion.section>
   );
