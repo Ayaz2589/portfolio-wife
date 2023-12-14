eb7592d9b6bfd41ed5f169fb1aba1fc30b9d7360
Ayaz Uddin <ayazuddin@ayazs-air.mynetworksettings.com>
2023-12-14 11:42:41 -0500

    dusable button

--- components/submit-btn/index.tsx
+++ components/submit-btn/index.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import React from "react";
 import { FaPaperPlane } from "react-icons/fa";
@@ -11,7 +11,7 @@ export default function SubmitBtn() {
     <button
       type="submit"
       className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 dark:bg-white dark:bg-opacity-10 disabled:scale-100 disabled:bg-opacity-65"
-      disabled={pending}
+      disabled={true} // until everything works
     >
       {pending ? (
         <div className="h-5 w-5 animate-spin rounded-full border-b-2 border-white"></div>
