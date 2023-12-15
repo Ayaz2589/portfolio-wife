be5d6948214d99532bac1ae39d9dd1086387470d
Ayaz Uddin <ayazuddin@ayazs-air.mynetworksettings.com>
2023-12-15 11:15:28 -0500

    update

--- components/projects/project.tsx
+++ components/projects/project.tsx
@@ -12,6 +12,8 @@ export default function Project({
   description,
   imageUrl,
   tags,
+  link,
+  repository,
 }: ProjectProps) {
   const ref = useRef<HTMLDivElement>(null);
   const { scrollYProgress } = useScroll({
@@ -26,11 +28,11 @@ export default function Project({
       style={{ scale, opacity }}
       className="mb-3 sm:mb-8 last:mb-0"
     >
-      <section className="bg-gray-100/60 m-auto max-w-[95%] sm:max-w-[42rem] overflow-hidden sm:pr-8 relative sm:h-[20rem] shadow-sm hover:shadow-xl transition group-even:pl-8 rounded-xl cursor-pointer">
+      <section className="bg-gray-100/60 m-auto max-w-[95%] sm:max-w-[42rem] overflow-hidden sm:pr-8 relative sm:h-[26rem] shadow-sm transition group-even:pl-8 rounded-xl">
         <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem]">
           <h3 className="text-2xl text-left font-semibold">{title}</h3>
           <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
-          <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
+          <ul className="flex flex-wrap gap-2 sm:mt-auto mt-4">
             {tags.map((tag) => (
               <li
                 key={tag}
@@ -40,6 +42,37 @@ export default function Project({
               </li>
             ))}
           </ul>
+          <div className="mt-10">
+            {title === "Node Services" ? (
+              <button
+                type="button"
+                className="text-white bg-gray-800  focus:outline-none focus:ring-4 focus:ring-gray-300 text-xs rounded-full px-4 py-2 me-2 mb-2 dark:bg-gray-800 dark:focus:ring-gray-700 dark:border-gray-700"
+                disabled
+              >
+                <div className="flex gap-2 items-center">
+                  <p>Service is live</p>
+                  <div className="w-[5px] h-[5px] rounded-full bg-green-500"></div>
+                </div>
+              </button>
+            ) : (
+              <a href={link} target="_blank">
+                <button
+                  type="button"
+                  className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 text-xs rounded-full px-4 py-2 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+                >
+                  View Live App
+                </button>
+              </a>
+            )}
+            <a href={repository} target="_blank">
+              <button
+                type="button"
+                className="text-grap-700 bg-gray-200 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 text-xs rounded-full px-4 py-2 me-2 mb-2 border border-gray-200/30 dark:bg-gray-200/30 dark:hover:bg-white dark:focus:ring-gray-700 dark:border-gray-700"
+              >
+                View Repository
+              </button>
+            </a>
+          </div>
         </div>
         <Image
           src={imageUrl}
